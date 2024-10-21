df43bfb8d51d28d6198e808ddab06d50b5444b12
marc <marcpascual@ceroca.cat>
2024-10-21 18:00:00 +0200

    botons principals

---
+++ Assets/Scripts/pantallainici.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pantallainici : MonoBehaviour
+{
+    public void pantallajuego()
+    {
+       SceneManager.LoadScene("juego");
+    }
+}
