28c84cadd1722a480f9025a59649026b59db4a56
Ana WU <ana_wu@ceroca.cat>
2024-11-14 15:59:56 +0100

    MergeModificar

---
+++ Assets/Sprites/Scripts/OperacionMatematica.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  // Necesario para cambiar de escena (nivel)
+
+public class OperacionMatematica : MonoBehaviour
+{
+    
+    public float numero1 = 10f;
+    public float numero2 = 5f;
+    public int resultadoOperacion; 
+    public int puntajeActual = 0;  
+    public int puntajeRequeridoParaSiguienteNivel = 50;  
+    private bool nivelCompletado = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        RealizarOperacionMatematica();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (puntajeActual >= puntajeRequeridoParaSiguienteNivel && !nivelCompletado)
+        {
+            PasarAlSiguienteNivel();
+        }
+    }
+
+ 
+    void RealizarOperacionMatematica()
+    {
+        
+        resultadoOperacion = (int)(numero1 * numero2);
+
+        
+        if (resultadoOperacion == 50)
+        {
+            
+            puntajeActual += 10;  
+            Debug.Log("¡Operación correcta! Puntaje actual: " + puntajeActual);
+        }
+        else
+        {
+            Debug.Log("Operación incorrecta. El resultado era: " + resultadoOperacion);
+        }
+    }
+
+    
+    void PasarAlSiguienteNivel()
+    {
+        
+        nivelCompletado = true;
+
+   
+        Debug.Log("¡Nivel completado! Pasando al siguiente nivel...");
+
+        
+        SceneManager.LoadScene("SiguienteNivel");  
+    }
+
+    
+    void Sumar()
+    {
+        resultadoOperacion = (int)(numero1 + numero2); // Realiza la suma
+        Debug.Log("Resultado de la suma: " + resultadoOperacion);
+    }
+
+ 
+    void Restar()
+    {
+        resultadoOperacion = (int)(numero1 - numero2); // Realiza la resta
+        Debug.Log("Resultado de la resta: " + resultadoOperacion);
+    }
+}
