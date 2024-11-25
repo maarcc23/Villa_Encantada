418c5e5b9aed52ca0f8094687f9af55daaa132ef
Ana WU <ana_wu@ceroca.cat>
2024-11-25 20:33:28 +0100

    PersonatgeTocarPorta

--- Assets/Scripts/OperacionMatematica.cs
+++ Assets/Scripts/OperacionMatematica.cs
@@ -5,47 +5,48 @@ using UnityEngine.SceneManagement;  // Necesario para cambiar de escena (nivel)
 
 public class OperacionMatematica : MonoBehaviour
 {
-    
+
     public float numero1 = 10f;
     public float numero2 = 5f;
-    public int resultadoOperacion; 
-    public int puntajeActual = 0;  
-    public int puntajeRequeridoParaSiguienteNivel = 50;  
+    public int resultadoOperacion;
+    public int puntajeActual = 0;
+    public int puntajeRequeridoParaSiguienteNivel = 50;
     private bool nivelCompletado = false;
+    public string tagPuerta = "Puerta";
 
     // Start is called before the first frame update
     void Start()
     {
-      
-        RealizarOperacionMatematica();
+
+        //RealizarOperacionMatematica();
     }
 
     // Update is called once per frame
     void Update()
     {
        
-        if (puntajeActual >= puntajeRequeridoParaSiguienteNivel && !nivelCompletado)
-        {
-            PasarAlSiguienteNivel();
-        }
     }
 
- 
-    void RealizarOperacionMatematica()
+
+    public void RealizarOperacionMatematica()
     {
-        
+
         resultadoOperacion = (int)(numero1 * numero2);
 
-        
+
         if (resultadoOperacion == 50)
         {
-            
-            puntajeActual += 10;  
+
+            puntajeActual += 10;
             Debug.Log("¡Operación correcta! Puntaje actual: " + puntajeActual);
         }
         else
         {
             Debug.Log("Operación incorrecta. El resultado era: " + resultadoOperacion);
+        }
+        if (puntajeActual >= puntajeRequeridoParaSiguienteNivel && !nivelCompletado)
+        {
+            PasarAlSiguienteNivel();
         }
     }
 
@@ -53,13 +54,12 @@ public class OperacionMatematica : MonoBehaviour
     void PasarAlSiguienteNivel()
     {
         
-        nivelCompletado = true;
+      nivelCompletado = true;
 
-   
         Debug.Log("¡Nivel completado! Pasando al siguiente nivel...");
 
-        
-        SceneManager.LoadScene("SiguienteNivel");  
+        // Cambia a la escena siguiente
+        SceneManager.LoadScene("SiguienteNivel");
     }
 
     
--- Assets/Scripts/Personatge1.cs
+++ Assets/Scripts/Personatge1.cs
@@ -9,10 +9,10 @@ public class Personatge1 : MonoBehaviour
     private float _velocitatCorre;
     private float _velocitatSaltar;
 
-    public Transform _centreZonaContacteTerra;
     public float _radiContacteTerra;
     public LayerMask _layereTerra;
     private bool _tocaTerra;
+    public float _velocitatVertical;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +21,7 @@ public class Personatge1 : MonoBehaviour
         _rigidbody2D.freezeRotation = true;
         _velocitatCorre = 5f;
         _velocitatSaltar = 5f;
+        _velocitatVertical = 3f;
         _tocaTerra = false;
 
     }
@@ -29,10 +30,10 @@ public class Personatge1 : MonoBehaviour
     void Update()
 
     {
-        _tocaTerra = Physics2D.OverlapCircle(_centreZonaContacteTerra.position, _radiContacteTerra, _layereTerra);
+        //_tocaTerra = Physics2D.OverlapCircle(_centreZonaContacteTerra.position, _radiContacteTerra, _layereTerra);
 
-        Debug.DrawRay(_centreZonaContacteTerra.position, Vector2.down, Color.green);
-        float inputHorizontal = Input.GetAxisRaw("Horizontal")*_velocitatCorre;
+        //Debug.DrawRay(_centreZonaContacteTerra.position, Vector2.down, Color.green);
+        float inputHorizontal = Input.GetAxisRaw("Horizontal") * _velocitatCorre;
         float inputVertical = Input.GetAxisRaw("Vertical") * _velocitatCorre;
 
         /*if (Input.GetKeyDown(KeyCode.Space))
@@ -41,6 +42,36 @@ public class Personatge1 : MonoBehaviour
             _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x,_velocitatSaltar);
         }*/
 
-        _rigidbody2D.velocity = new Vector2(inputHorizontal,_rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = new Vector2(inputHorizontal, inputVertical);
+    }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+
+    {
+        if (collision.CompareTag("NoTocar"))
+        {
+            Muerte();
+            Debug.Log("¡Has tocado un objeto NoTocar!");
+        }
+
+        if (collision.CompareTag("PuertaGame3"))
+        {
+            Debug.Log("¡Has tocado la puerta!");
+            GameObject.Find("fondojuego3").GetComponent<OperacionMatematica>().RealizarOperacionMatematica();
+        }
+    }
+    private void Muerte()
+    {
+       
+        gameObject.SetActive(false);
+
+       
+        Invoke("ReiniciarNivel", 2f);  
+    }
+
+    
+    private void ReiniciarNivel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
     }
 }
